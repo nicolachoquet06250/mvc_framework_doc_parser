780879897c62974e0a0ec3d95394207e47ad2d09
nicolas <nicolachoquet06250@gmail.com>
2018-10-19 00:32:10 +0200

    amélioration de quelques fonctionnalitées et début du développement de la génération de la doc css ( parsage terminé ) -> à faire : mise en page dans le template blade avec bootstrap

--- Parser.php
+++ Parser.php
@@ -3,18 +3,26 @@
 namespace mvc_framework\core\doc_parser;
 
 
-class DocParser {
+class Parser {
 	private $type;
 
 	public function __construct($type) {
 		$this->type = $type;
 	}
 
+	/**
+	 * @return mixed
+	 * @throws \Exception
+	 */
 	public function parse() {
-		if(file_exists(__DIR__.'/'.ucfirst($this->type).'.php')) {
-			require_once __DIR__.'/'.ucfirst($this->type).'.php';
-			$parser_class = '\mvc_framework\core\doc_parser\\'.ucfirst($this->type);
+		if(file_exists(__DIR__.'/'.ucfirst($this->type).'Parser.php')) {
+			require_once __DIR__.'/'.ucfirst($this->type).'Parser.php';
+			$parser_class = '\mvc_framework\core\doc_parser\\'.ucfirst($this->type).'Parser';
+			/**
+			 * @var Parser $parser;
+			 */
 			$parser = new $parser_class();
+			return $parser->parse();
 		}
 		else {
 			throw new \Exception($this->type.' parser not expected !');
---
+++ ScssParser.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace mvc_framework\core\doc_parser;
+
+
+use mvc_framework\core\paths\Path;
+
+class ScssParser {
+	private $doc = [],
+		$inline = ['author', 'date', 'title'];
+
+	public function parse() {
+		$scss_source = Path::get('core_doc-parser_parsers-conf_sass');
+		$this->parse_part1($scss_source);
+		$this->parse_part2();
+		$this->parse_part3();
+		$this->parse_part4();
+		$this->parse_part5();
+		return $this->doc;
+	}
+
+	private function parse_part1($scss_source) {
+		$dir = opendir($scss_source);
+		while (($file = readdir($dir)) !== false) {
+			if($file !== '.' && $file !== '..') {
+				$content = file_get_contents($scss_source.'/'.$file);
+				$path_to_kipe = explode('/', $scss_source);
+				$path_to_kipe = [
+					$path_to_kipe[count($path_to_kipe)-3],
+					$path_to_kipe[count($path_to_kipe)-2],
+					$path_to_kipe[count($path_to_kipe)-1],
+				];
+				$this->doc[implode('/', $path_to_kipe).'/'.$file] = $content;
+			}
+		}
+	}
+	private function parse_part2() {
+		foreach ($this->doc as $id => $doc) {
+			if(preg_match('`\/[\*]{2,2}\n([^\*]+)\*\/`', $doc, $matches)) {
+				$doc_content = $matches[1];
+				$doc_content = explode("\n@", $doc_content);
+				$this->doc[$id] = $doc_content;
+			}
+		}
+	}
+	private function parse_part3() {
+		foreach ($this->doc as $id => $doc) {
+			foreach ($doc as $_id => $doc_part) {
+				if(substr($doc_part, 0, 1) === '@') {
+					$this->doc[$id][$_id] = substr($doc_part, 1, strlen($doc_part)-1);
+				}
+			}
+		}
+	}
+	private function parse_part4() {
+		foreach ($this->doc as $id => $doc) {
+			$doc_tmp = [];
+			foreach ($doc as $_id => $doc_part) {
+				if(in_array(explode(' ', $doc_part)[0], $this->inline)) {
+					$key = explode(' ', $doc_part)[0];
+					$content = explode(' ', $doc_part);
+					unset($content[0]);
+					$content_tmp = [];
+					foreach ($content as $c) {
+						if($c !== '' && $c !== null) {
+							$content_tmp[] = $c;
+						}
+					}
+					$content = $content_tmp;
+					$doc_tmp[$key] = str_replace("\n", '', implode(' ', $content));
+				}
+				else {
+					$key = explode("\n", $doc_part)[0];
+					$content = explode("\n", $doc_part);
+					unset($content[0]);
+					$content_tmp = [];
+					foreach ($content as $c) {
+						if($c !== '' && $c !== null) {
+							$content_tmp[] = $c;
+						}
+					}
+					$content = $content_tmp;
+					$doc_tmp[$key] = implode("\n", $content);
+				}
+			}
+			$this->doc[$id] = $doc_tmp;
+		}
+	}
+	private function parse_part5() {
+		foreach ($this->doc as $id => $doc) {
+			$modifiers = $doc['modifiers'];
+			$modifiers = explode("\n", $modifiers);
+			$modifiers_tmp = [];
+			foreach ($modifiers as $modifier) {
+				$modifier = explode(' - ', $modifier);
+				$modifiers_tmp[$modifier[0]] = $modifier[1];
+			}
+			$modifiers = $modifiers_tmp;
+			$this->doc[$id]['modifiers'] = $modifiers;
+		}
+	}
+}
